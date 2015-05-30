73634276daa0be78b124cce8db836ca72b920004
Daniel Lucas <me@daniellucas.net>
2015-05-30 02:14:30 -0300

    vendor compiled assets

---
+++ public/assets/catarse_pagarme/application-6e0f4db255ecc02d48999b265345598c.js
@@ -0,0 +1,1 @@
+App.addChild("Pagarme",{el:"#catarse_pagarme_form",activate:function(){this.message=this.$(".next_step_after_valid_document .alert-danger"),this.loader=this.$(".loader img"),this.contributionId=$("input#contribution_id").val(),this.projectId=$("input#project_id").val()}});
